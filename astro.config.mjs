82d302001e4ac0939f04fce9dc8994328176ac53
agimenezromero <gimenez.romero.alex@gmail.com>
2026-07-25 20:48:58 +0200

    Improved sitemap and removed robots duplicates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 const site = process.env.PUBLIC_SITE_URL || 'https://breda-accio-solidaria.github.io';
 
 export default defineConfig({
   site,
   output: 'static',
   trailingSlash: 'always',
+
   image: {
     responsiveStyles: true,
   },
+
+  integrations: [sitemap()],
 });
